b530246a9cda3d7a1d3a1fbb0770ef50bed8b2e2
surajbca <krsurajkumar7481@gmail.com>
2023-10-17 13:20:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function showRow() {
+  const inputData = document.getElementById("inputData");
+  inputData.style.display = "";
+}
+
+function HideRow() {
+  const inputData = document.getElementById("inputData");
+  inputData.style.display = "none";
+}
